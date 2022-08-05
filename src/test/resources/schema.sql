5e794d4e7ec79f1605becc7d997d90451b96a281
YunChen <1183728554@qq.com>
2022-08-05 23:32:11 +0800

    feat:sql init schema-locations"

--- src/test/resources/schema.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS `todo`;
-CREATE TABLE `todo` (
-                        `id` VARCHAR(100) NOT NULL ,
-                        `text` VARCHAR(100) NULL DEFAULT '',
-                        `done` TINYINT(1) NULL DEFAULT '0',
-                        PRIMARY KEY (`id`)
-);
