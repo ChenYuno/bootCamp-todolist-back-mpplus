c3ba4b5bdc7c4f46f4bf90b8d3495c1802f3f0b6
YunChen <1183728554@qq.com>
2022-08-05 22:45:16 +0800

    feat:ready for depoly heroku

--- src/main/resources/db/migration/V20220804133603__create_table_todo.sql
+++ src/main/resources/db/migration/V20220804133603__create_table_todo.sql
@@ -1,8 +1,5 @@
 CREATE TABLE if not exists `todo` (
-                        `id` VARCHAR(100) NOT NULL COLLATE 'utf8_general_ci',
-                        `text` VARCHAR(100) NULL DEFAULT '' COLLATE 'utf8_general_ci',
-                        `done` TINYINT(1) NULL DEFAULT '0',
-                        PRIMARY KEY (`id`) USING BTREE
-)
-    COLLATE='utf8_general_ci'
-ENGINE=InnoDB
+                        `id` VARCHAR(100) NOT NULL PRIMARY KEY,
+                        `text` VARCHAR(100) NULL DEFAULT '',
+                        `done` boolean
+);
