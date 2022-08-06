a9a8267ca62be00aa88b1f9fc1dfa6b5814bcd1e
YunChen <1183728554@qq.com>
2022-08-07 03:45:36 +0800

    test

--- src/main/resources/db/migration/V20220804133623__insert_todo_data.sql
+++ src/main/resources/db/migration/V20220804133623__insert_todo_data.sql
@@ -1,7 +1,7 @@
-truncate table todo;
-INSERT INTO todo (id,text,done) VALUES
-                                               ('1555059179989770242','text2',false),
-                                               ('1555059179989770241','移除pipeline试试',true),
-                                                ('1555059179989770243','还在pipeline试试',false),
-                                               ('1555059532848168962','text333',true);
+-- truncate table todo;
+-- INSERT INTO todo (id,text,done) VALUES
+--                                                ('1555059179989770242','text2',false),
+--                                                ('1555059179989770241','移除pipeline试试',true),
+--                                                 ('1555059179989770243','还在pipeline试试',false),
+--                                                ('1555059532848168962','text333',true);
 
