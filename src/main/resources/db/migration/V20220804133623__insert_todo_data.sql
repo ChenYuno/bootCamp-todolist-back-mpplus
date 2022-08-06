f745f5d1abfdc704fc8695e1b3dfca6d766a5689
YunChen <1183728554@qq.com>
2022-08-07 02:27:36 +0800

    test remove pipeline

--- src/main/resources/db/migration/V20220804133623__insert_todo_data.sql
+++ src/main/resources/db/migration/V20220804133623__insert_todo_data.sql
@@ -1,4 +1,5 @@
 INSERT INTO todo (id,text,done) VALUES
                                                ('1555059179989770242','text2',false),
+                                               ('1555059179989770242','移除pipeline试试',true),
                                                ('1555059532848168962','text333',true);
 
