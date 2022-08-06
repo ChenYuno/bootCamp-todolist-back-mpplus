e0ff10a9f8d1268795536c42c9569c5ea109fe25
YunChen <1183728554@qq.com>
2022-08-07 00:29:46 +0800

    feat:de try again

--- src/main/resources/db/migration/V20220804133623__insert_todo_data.sql
+++ src/main/resources/db/migration/V20220804133623__insert_todo_data.sql
@@ -1,4 +1,4 @@
-INSERT INTO todolist.todo (id,`text`,done) VALUES
+INSERT INTO todolist.todo (id,text,done) VALUES
                                                ('1555059179989770242','text2',0),
                                                ('1555059532848168962','text333',0);
 
